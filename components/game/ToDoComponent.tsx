90caf9d2f332fa0e672ba8580dc9cecd453469dd
Eliott <eliott.oblinger7@etu.univ-lorraine.fr>
2022-12-07 16:32:50 +0100

    Add: several components

---
+++ components/game/ToDoComponent.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+function ToDoComponent(){
+  return (
+    <View>
+      <Text>
+        Test
+      </Text>
+    </View>
+  )
+}
+
+export default ToDoComponent;
---
+++ components/players/PlayersComponent.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+function PlayersComponent(){
+  return (
+    <View>
+      <Text>
+        Test
+      </Text>
+    </View>
+  )
+}
+
+export default PlayersComponent;
