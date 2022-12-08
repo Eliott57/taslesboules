dbb5514e6960d332670eede11b23a10d43679728
laaouine <72995750+laaouine@users.noreply.github.com>
2022-12-08 10:57:45 +0100

    juste pour sauvegarder mes changement
    je switch l'éditeur don j'ai fais ce commit pour sauvegarder les changement

--- components/players/PlayersComponent.tsx
+++ components/players/PlayersComponent.tsx
@@ -1,13 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import fondExpo from "../assets/Fond-export_3.svg";
+import {SvgCssUri} from 'react-native-svg';
+import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
+
+
+
+const svg = resolveAssetSource(fondExpo);
 
 function PlayersComponent(){
   return (
     <View>
       <Text>
-        Test
+        <SvgCssUri style={styles.background} uri={svg.uri} width="105%" height="105%" />
+        <input type text> value="LastName-FirstName"</input>
+        <input type="submit" value="+" ></input>
       </Text>
     </View>
   )
+}
+
+const styles{
+
+.background{
+    top: -30;
+}
 }
 
 export default PlayersComponent;
