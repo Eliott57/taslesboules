36cc614312945719f903ecf7f56678bdb8f6ebd2
IC-faycal <faycal@inovea-conseil.com>
2022-12-08 08:51:46 +0100

    NEW build home page

--- components/HomeComponent.tsx
+++ components/HomeComponent.tsx
@@ -1,13 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import Home from '../assets/Home.svg';
+import HomePlay from '../assets/Home-play.svg';
+import HomeHall from '../assets/Home-hall.svg';
+import {SvgCssUri} from 'react-native-svg';
+import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
+
+const svg = resolveAssetSource(Home);
+const svgButPlay = resolveAssetSource(HomePlay);
+const svgButHall = resolveAssetSource(HomeHall);
 
 function HomeComponent(){
   return (
     <View>
-      <Text>
-        Test
-      </Text>
+        <SvgCssUri style={styles.btnPlay} width="60%" height="60%" uri={svgButPlay.uri} />
+        <SvgCssUri style={styles.btnHall} width="22%" height="22%" uri={svgButHall.uri} />
+        <SvgCssUri style={styles.back} uri={svg.uri} width="105%" height="105%" />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  btnPlay: {
+    position: 'absolute',
+    top: 95,
+    left: 90,
+    zIndex: 1,
+  },
+  btnHall: {
+    position: 'absolute',
+    bottom: 40,
+    right: 20,
+    zIndex: 1,
+  },
+  back: {
+    top: -30
+  },
+});
 
 export default HomeComponent;
