9d8cb08d4f1a42dc66e99373c2f4504586d32abb
laaouine <72995750+laaouine@users.noreply.github.com>
2022-12-09 11:54:50 +0100

    QuestionStats

--- components/stats/StatsComponent.tsx
+++ components/stats/StatsComponent.tsx
@@ -2,6 +2,8 @@ import { View, Text } from "react-native";
 import OptionsComponent from '../questions/OptionsComponent.tsx';
 import OptionButtonComponent from '../questions/OptionButtonComponent.tsx';
 
+
+
 function StatsComponent(){
   return (
     <View>
---
+++ components/stats/questionComponent.tsx
@@ -0,0 +1,29 @@
+/*import text from "react-native";
+import backgroundpic from "../../assets/backstat.svg";
+
+
+<ImageBackground source={backgroundpic} style={{width: '100%', height: '100%',  alignItems: 'center'}}>
+
+function QuestionComponent(){
+
+
+
+    return (
+        <View>
+            <Text> Tu préfére ? </Text>
+            <div>
+                Question : {Iquestion}
+                Question : {Iquestion}
+            </div>
+             <OptionButtonComponent label="A" />
+        </View>
+      )
+      const style = {
+        text: {
+            fontSize: 25;
+            color: red;
+        }
+      }
+}
+
+export default QuestionComponent;*/
